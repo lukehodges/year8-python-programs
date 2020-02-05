bb197f2dc3f656813b15426b0ce8aee33f56f937
lukehodges <58270858+lukehodges@users.noreply.github.com>
2020-02-05 20:12:53 +0000

    Update Finding volume.py

--- Finding volume.py
+++ Finding volume.py
@@ -1,25 +1,34 @@
+# importing math for square routing (line 24)
 import math
+#making nececary variables
 radius = 2
 pi = 3.14159265358979
 height = 3
 line = height
+
 def sphere(radius, pi):
-	pi2 = pi/4 *3
-	radius = radius*radius*radius
-	volume = pi2*radius
+	#volume = 3/4 pi * radius to the power of 3
+	pi2 = pi/4 *3 #3/4 of pi
+	radius = radius*radius*radius # cubing the radius
+	volume = pi2*radius 
 	print(volume)
 	volume = 0
 
 def cone(radius, height, pi):
+	#volume = height/3 * pi * radius squared
 	height = height/3
 	radius = radius*radius
 	volume = pi*radius*height
 	print(volume)
 
 def cylinder(radius, height, pi):
+	# volume = pi * radius squared * heightt
 	volume = pi*radius*radius*height
 	print(volume)
+	
 def d20(line):
+	#volume = 5(3+squrt(5)) / 12 * line cubed
+	#line is the length of one of the sides
 	volume = math.sqrt(5)
 	volume = volume + 3
 	volume = volume*5
@@ -28,6 +37,7 @@ def d20(line):
 	volume = volume*line
 	print(volume)
 
+#remove hash to run
 #cylinder(radius, height, pi)
-d20(line)
+#d20(line)
 #sphere(radius, pi)
